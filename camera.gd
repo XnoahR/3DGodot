09257773f13b040bee56a4e620b9740c5f4943a1
Rio Saputro <rio.ae23@gmail.com>
2024-01-01 17:21:12 +0700

    Initial commit

---
+++ camera.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+var camera
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	camera = $Camera3D
+	pass # Replace with function body.
+
+func _camera_move(delta):
+	if(Input.is_action_pressed("ui_up")):
+		camera.transform.origin += Vector3(0,0,5*delta)
+	if(Input.is_action_pressed("ui_down")):
+		camera.transform.origin += Vector3(0,-0.1,0)
+	if(Input.is_action_pressed("ui_left")):
+		camera.transform.origin += Vector3(-0.1,0,0)
+	if(Input.is_action_pressed("ui_right")):
+		camera.transform.origin += Vector3(0.1,0,0)
+	
+#s
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	_camera_move(delta)
+	pass
