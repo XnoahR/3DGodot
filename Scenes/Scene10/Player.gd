ab41c1021ed5fdf7ecbd7988eefe64fed4ac78b0
Rio Saputro <rio.ae23@gmail.com>
2024-01-07 18:54:56 +0700

    C13 - Manual rotation and move with direction

--- Scenes/Scene10/Player.gd
+++ Scenes/Scene10/Player.gd
@@ -2,41 +2,30 @@ extends CharacterBody3D
 
 var acceleration = Vector3(0,0,0)
 @export var speed := 5.0
-
-
+@export var rad = 0
 
 func _physics_process(delta):
 	
 	acceleration = Vector3.ZERO
-	if(Input.is_action_pressed("ui_left")):
-		#Move Left
-		acceleration.x = -1
-		#I want rotate/look left here
-		#look_at_from_position(transform.origin,Vector3(-1,0,0),Vector3.UP)
-		#look_at(Vector3(-1,0,0),Vector3.UP,true)
-		look_at(position + Vector3(1, 0, 0), Vector3.UP)
-	if(Input.is_action_pressed("ui_right")):
-		#Move right
-		acceleration.x = 1
-		look_at(position + Vector3(-1, 0, 0), Vector3.UP)
-		#look_at(Vector3(1,0,0),Vector3.UP,true)
-		#look_at_from_position(transform.origin,Vector3(1,0,0),Vector3.UP)
-	if(Input.is_action_pressed("ui_up")):
-		#Move front
-		acceleration.z = 1
-		look_at(position + Vector3(0, 0, -1), Vector3.UP)
-		#look_at_from_position(transform.origin,Vector3(0,0,0),Vector3.UP)
-	if(Input.is_action_pressed("ui_down")):
-		#Move back
-		acceleration.z = -1
-		look_at(position + Vector3(0, 0, 1), Vector3.UP)
-		#look_at_from_position(transform.origin,Vector3(0,0,-1),Vector3.UP)
+	acceleration.x = Input.get_axis("ui_left","ui_right")
+	acceleration.z = Input.get_axis("ui_up","ui_down")
+
+	var offset = 0.02
+	if(Input.is_action_pressed("left_rad")):
+		rad -= offset
+		rotate_y(-offset)
 		
+	if(Input.is_action_pressed("right_lad")):
+		rad += offset
+		rotate_y(offset)
+	
+	#look_at(position + -acceleration,Vector3.UP)
+	acceleration = acceleration.rotated(Vector3.UP,rad)
 	velocity = acceleration*speed
 	if((velocity.length() != 0) and ($AnimationPlayer.current_animation != "Standard Walk")):
 		$AnimationPlayer.play("Standard Walk")
 	elif ((velocity.length() == 0) and ($AnimationPlayer.current_animation != "Standard Idle")):
 		$AnimationPlayer.play("Standard Idle")
-		
+	
 	move_and_slide()
 	pass
